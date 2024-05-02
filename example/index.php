2dc0d0bdc46ed7258449dbc1f44dffaa1798169d
FRANCESCO BIAGINI <info@hanamisoftware.com>
2024-05-02 09:34:28 +0200

    Update files and template

--- example/index.php
+++ example/index.php
@@ -14,36 +14,36 @@
                   <img class="logo" src="img/cagnetto-direzione.png" />
               </div>
               <div class="col-md.12 header-text text-center">
-                  <h1>Sito in Costruzione</h1>
+                  <h1>We are Building it</h1>
               </div>
           </div>
           <div class="container card-container">
               <div class="row content center">
                   <div class="card-sx col-md-5">
-                      <div class="card-title">Telefono</div>
+                      <div class="card-title">Card Title</div>
                       <div class="card-content">
-                          <p class="card-content-element">Giorni feriali dalle 08:00 alle 20:00</p>
-                          <p class="card-content-element">000 0000 000</p>
-                          <p class="card-content-element">Giorni festivi dalle 08:00 alle 20:00</p>
-                          <p class="card-content-element">000 0000 000</p>
-                          <p class="card-content-element">Notturno dalle 20:00 alle 08:00</p>
-                          <p class="card-content-element">112 - 113</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
                       </div>
                   </div>
                   <div class="card-dx col-md-5">
-                      <div class="card-title">Orari</div>
+                      <div class="card-title">Card Title</div>
                       <div class="card-content">
-                          <p class="card-content-element">Primavera - Estate</p>
-                          <p class="card-content-element">14:30 - 18:30</p>
-                          <p class="card-content-element">Autunno - Inverno</p>
-                          <p class="card-content-element">12:30 - 16:30</p>
-                          <p class="card-content-element">Chiuso la Domenica e i giorni festivi</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
+                          <p class="card-content-element">card-content-element</p>
                       </div>
                   </div>
               </div>
           </div>
           <div class="row footer text-center">
-              <p class="footer-text">Copyright &copy; <?php echo date('Y'); ?> A.C.P.A Cesena tutti i diritti riservati</p>
+              <p class="footer-text">Copyright &copy; <?php echo date('Y'); ?> Hanami Software</p>
               <p class="footer-text">Credits: <a href="//www.hanamisoftware.com">Hanami Software</a></p>
           </div>
       </div>
