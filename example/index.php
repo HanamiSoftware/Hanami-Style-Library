6fc4cd84a0ebcbf230ae6f5d734c904dd9df8f01
FRANCESCO BIAGINI <info@hanamisoftware.com>
2024-05-02 02:54:38 +0200

    Aggiungere i file di progetto.

---
+++ example/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ACPA Canile Intercomunale di Cesena</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet" />
+  </head>
+  <body>
+      <div class="container">
+          <div class="row header">
+              <div class="col-md-12 text-center">
+                  <img class="logo" src="img/cagnetto-direzione.png" />
+              </div>
+              <div class="col-md.12 header-text text-center">
+                  <h1>Sito in Costruzione</h1>
+              </div>
+          </div>
+          <div class="container card-container">
+              <div class="row content center">
+                  <div class="card-sx col-md-5">
+                      <div class="card-title">Telefono</div>
+                      <div class="card-content">
+                          <p class="card-content-element">Giorni feriali dalle 08:00 alle 20:00</p>
+                          <p class="card-content-element">000 0000 000</p>
+                          <p class="card-content-element">Giorni festivi dalle 08:00 alle 20:00</p>
+                          <p class="card-content-element">000 0000 000</p>
+                          <p class="card-content-element">Notturno dalle 20:00 alle 08:00</p>
+                          <p class="card-content-element">112 - 113</p>
+                      </div>
+                  </div>
+                  <div class="card-dx col-md-5">
+                      <div class="card-title">Orari</div>
+                      <div class="card-content">
+                          <p class="card-content-element">Primavera - Estate</p>
+                          <p class="card-content-element">14:30 - 18:30</p>
+                          <p class="card-content-element">Autunno - Inverno</p>
+                          <p class="card-content-element">12:30 - 16:30</p>
+                          <p class="card-content-element">Chiuso la Domenica e i giorni festivi</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="row footer text-center">
+              <p class="footer-text">Copyright &copy; <?php echo date('Y'); ?> A.C.P.A Cesena tutti i diritti riservati</p>
+              <p class="footer-text">Credits: <a href="//www.hanamisoftware.com">Hanami Software</a></p>
+          </div>
+      </div>
+    <!-- Loading scripts-->
+      <script type="text/javascript" async src="vendor/components/jquery/jquery.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
